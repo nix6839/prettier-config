9b5ccbe7400f9750f6fec1d63a160985bed22a85
Han Yeong-woo <han@yeongwoo.dev>
2022-06-21 08:04:52 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# @nix/prettier-config
+
+Yeong-Woo Han's extensible prettier config
+
+## Usage
+
+### 1. Install prettier and this package
+
+```sh
+npm i -D @nix/prettier-config prettier
+```
+
+### 2. Add `@nix/prettier-config` to your `.prettierrc`
---
+++ index.ts
@@ -0,0 +1,44 @@
+import {Config, Options, RequiredOptions} from 'prettier';
+
+type PrettierOptions = Omit<
+  RequiredOptions,
+  // Depreacted options.
+  | 'jsxBracketSameLine' // use bracketSameLine insted
+  // Options that don't have default value.
+  | 'parser'
+  | 'filepath'
+  | 'plugins'
+  | 'pluginSearchDirs'
+  | 'parentParser'
+  | '__embeddedInHtml'
+>;
+
+type PrettierConfig = PrettierOptions & Omit<Config, keyof Options>;
+
+const config: PrettierConfig = {
+  // Overridden options
+  singleQuote: true,
+  trailingComma: 'all',
+  // Options with default values
+  semi: true,
+  jsxSingleQuote: false,
+  bracketSpacing: false,
+  bracketSameLine: false,
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: 'preserve',
+  arrowParens: 'always',
+  htmlWhitespaceSensitivity: 'css',
+  endOfLine: 'lf',
+  quoteProps: 'as-needed',
+  vueIndentScriptAndStyle: false,
+  embeddedLanguageFormatting: 'auto',
+  singleAttributePerLine: false,
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+};
+
+module.exports = config;
