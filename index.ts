4284dc03b90d65b5a490e7fc6eceb8dd25d2cedc
Han Yeong-woo <nix6839@washnix.com>
2023-08-14 22:32:41 +0900

    refactor: move code to default value

--- index.ts
+++ index.ts
@@ -19,11 +19,11 @@ type PrettierConfig = PrettierOptions & Omit<Config, keyof Options>;
 const config: PrettierConfig = {
   // Overridden options
   singleQuote: true,
-  trailingComma: 'all',
 
   // Options with default values
   semi: true,
   jsxSingleQuote: false,
+  trailingComma: 'all',
   bracketSpacing: true,
   bracketSameLine: false,
   rangeStart: 0,
